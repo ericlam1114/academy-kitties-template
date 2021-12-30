c3f951636f6b10652dfefefd581d6a285e360366
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2021-12-30 14:35:01 -0800

    Kitty Contract Completed

--- contracts/Kittycontract.sol
+++ contracts/Kittycontract.sol
@@ -1,14 +1,30 @@
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.0;
 
 import "./IERC721.sol";
+import "./Ownable.sol";
 
-abstract contract Kittycontract is IERC721 {
+abstract contract Kittycontract is IERC721, Ownable {
 
     string public constant catTokenName = "Cat";
     string public constant catSymbol = "CT";
 
+
     mapping(address => uint256) tokenOwnershipCount;
     mapping(uint256 => address) public tokenIdOwnerMapping;
+
+    uint256 public gen0Counter; 
+    uint256 public CREATION_LIMIT_GEN0 = 10; 
+
+    function createKittyGen0(uint256 _genes) public onlyOwner returns(uint256){
+        require(gen0Counter < CREATION_LIMIT_GEN0);
+
+        gen0Counter++;
+
+         return _createKitty(0, 0, 0, _genes, msg.sender);
+    }
+
+
+    event Birth(address owner, uint256 kittenId, uint256 mumId, uint256 dadId, uint256 genes);
 
 
     struct Cat {
@@ -16,11 +32,34 @@ abstract contract Kittycontract is IERC721 {
         uint64 birthTime;
         uint32 mumId;
         uint32 dadId;
-        uint16 generation;
+        uint256 generation;
     }
 
     Cat[] cats;
 
+    function _createKitty(
+        uint256 _mumId,
+        uint256 _dadId,
+        uint256 _generation,
+        uint256 _genes,
+        address _owner
+    ) public returns(uint256) {
+        Cat memory _cat = Cat({
+            genes: _genes, 
+            birthTime: uint64(block.timestamp),
+            mumId: uint32(_mumId), 
+            dadId: uint32(_dadId),
+            generation: uint16(_generation)
+        });
+         cats.push(_cat);
+         uint256 newCatId = cats.length - 1;
+
+         emit Birth(_owner, newCatId, _mumId, _dadId, _genes);
+
+         _transfer(address(0), _owner, newCatId);
+
+         return newCatId;
+    }
 
     function balanceOf(address owner) external view returns (uint256 balance){
         return tokenOwnershipCount[owner];
